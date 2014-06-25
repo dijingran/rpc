c6b36ed6226ebc4f9c3582cc1725c0f4b68a81e5
dixingxing <dixingxing@yeah.net>
2014-06-25 20:11:28 +0800

    实现代理类的toString方法，hashCode、equals等方法直接抛出异常。

--- rpc/src/main/java/org/dxx/rpc/client/ProxyFactory.java
+++ rpc/src/main/java/org/dxx/rpc/client/ProxyFactory.java
@@ -3,12 +3,15 @@ package org.dxx.rpc.client;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.dxx.rpc.EchoService;
 import org.dxx.rpc.Request;
 import org.dxx.rpc.ResponseFuture;
 import org.dxx.rpc.config.RpcClientConfig;
+import org.dxx.rpc.exception.RpcException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,6 +37,13 @@ public class ProxyFactory implements InvocationHandler {
 
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		if (ignoreMethod(method)) {
+			if ("toString".equals(method.getName())) {
+				return proxy.getClass().toString();
+			}
+			throw new RpcException("method not implement : " + method);
+		}
+
 		if (MockUtils.isMock(interfaceClass)) {
 			return MockUtils.invokeMockClass(interfaceClass, method, args);
 		}
@@ -56,5 +66,20 @@ public class ProxyFactory implements InvocationHandler {
 			throw e;
 		}
 		return f.get().getObj();
+	}
+
+	private static final List<String> ignoreMethods = new ArrayList<String>();
+
+	static {
+		ignoreMethods.add("toString");
+		ignoreMethods.add("hashCode");
+		ignoreMethods.add("equals");
+		ignoreMethods.add("wait");
+		ignoreMethods.add("notify");
+		ignoreMethods.add("notifyAll");
+	}
+
+	private static boolean ignoreMethod(Method m) {
+		return ignoreMethods.contains(m.getName());
 	}
 }
