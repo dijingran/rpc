ebe6179d75548d1adda084e5afbf874954026cc4
dijingran <dijingran@dijingran-PC>
2014-08-03 21:27:50 +0800

    修改worker线程池配置

--- rpc/src/main/java/org/dxx/rpc/server/exec/RpcChannelHandler.java
+++ rpc/src/main/java/org/dxx/rpc/server/exec/RpcChannelHandler.java
@@ -3,20 +3,23 @@ package org.dxx.rpc.server.exec;
 import io.netty.channel.Channel;
 
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import org.dxx.rpc.Request;
 
 public class RpcChannelHandler {
-	private static final NamedThreadFactory threadFactory = new NamedThreadFactory("rpc-exec-");
-
-	private static final ExecutorService executorService = Executors.newCachedThreadPool(new ThreadFactory() {
+	private static final ThreadFactory tf = new ThreadFactory() {
 		@Override
 		public Thread newThread(Runnable r) {
-			return threadFactory.newThread(r);
+			return new NamedThreadFactory("rpc-exec-").newThread(r);
 		}
-	});
+	};
+
+	private static final ExecutorService executorService = new ThreadPoolExecutor(0, 80, 60, TimeUnit.SECONDS,
+			new SynchronousQueue<Runnable>(), tf);
 
 	public void handle(Channel channel, Request request) {
 		executorService.execute(new RpcRunnable(channel, request));
